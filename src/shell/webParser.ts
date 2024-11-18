0363b176a733933b7c2dbaa404812e49bd3f51c7
Sivasai <sivasai.podugu@testsigma.com>
2024-11-19 00:48:38 +0530

    added dotenv in webparser

--- src/shell/webParser.ts
+++ src/shell/webParser.ts
@@ -1,5 +1,5 @@
 // This file replaces Parser.ts when curlconverter is running in the browser.
-
+require('dotenv').config({ path: '.env' });
 import Parser from "web-tree-sitter";
 
 // NOTE: top-level await requires Safari 15+
@@ -7,10 +7,10 @@ import Parser from "web-tree-sitter";
 await Parser.init({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   locateFile(scriptName: string, scriptDirectory: string) {
-    return import.meta.env.VITE_TREE_SITTER_PATH;
+    return process.env.VITE_TREE_SITTER_PATH;
   },
 });
-const Bash = await Parser.Language.load(import.meta.env.VITE_TREE_SITTER_BASH_PATH);
+const Bash = await Parser.Language.load(process.env.VITE_TREE_SITTER_BASH_PATH || '');
 const parser = new Parser();
 parser.setLanguage(Bash);
 
